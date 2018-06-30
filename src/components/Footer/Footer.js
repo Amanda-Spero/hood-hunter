8bbc9d21fc29f8d97d2f5e0adc2693fbf1881228
Veronica <chingona27>
2018-06-30 10:36:42 -0500

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+
+<footer className="page-footer blue-grey">
+                       
+          <div className="footer-copyright center-align">
+            <div className="container">
+            © 2018 Copyright Hood Hunter
+            </div>
+          </div>
+        </footer>
+            
+
+);
+
+export default Footer;
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => (
+  <div className="banner" >
+    <div className="card transparent">
+      <h1 className="header center orange-text ">Hood Hunter</h1>
+      <h5 className="header center col s12 light">Search your potential neighborhood below!</h5>   
+   </div>
+  </div>
+
+);
+
+export default Jumbotron;
