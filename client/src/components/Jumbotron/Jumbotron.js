2074ba66f542e68d5b7dc237a356cec5cb875d52
Veronica <chingona27>
2018-07-03 18:54:46 -0500

    updates

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Jumbotron = () => (
-  <div className="banner" >
-      <h1 className="header center orange-text ">Hood Hunter</h1>
-      <h5 className="header center s">Search your potential neighborhood below!</h5>   
+  <div className="card" >
+      <div className="card-image">
+      <img alt="city" src="https://cdn.theculturetrip.com/images/56-3940160-1441141742b47c175bf5824bf29f6cb0b91be2bd55.jpg" />
+      <span className="card-title">
+        <h1 className="header center amber-darken-4">Hood Hunter</h1>
+        <h5 className="header center s">Search your potential neighborhood below!</h5>  
+      </span>
+      </div> 
   </div>
 
 );
