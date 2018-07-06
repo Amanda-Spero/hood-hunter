d993d4a4ff442287ad429c8feb22edb4e81d6e45
crystalodi <crystalodi@gmail.com>
2018-07-06 13:36:26 -0500

    add actions and reducers for authentication and error logic for redux. Add API.js for calls related to authentication end points. Add setAuthenticationToken.js to Utils folder. Add validation folder for checking if there is a user in application state

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,51 @@
+import API from "../utils/API";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import  {GET_ERRORS, SET_CURRENT_USER} from "./types";
+//Register User
+export const registerUser = (userData, history) => dispatch => {
+    API.register(userData)
+    .then(response => history.push("/login"))
+    .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    )
+}
+
+//Login - Get User Token
+export const loginUser = userData => dispatch => {
+    API.login(userData)
+    .then(response => {
+        //save to local storage
+        const {token} = response.data;
+        localStorage.setItem("jwtToken", token)
+        //Set token to auth header
+        setAuthToken(token)
+        //Decode token to get user data
+        const decoded = jwt_decode(token);
+        //set current user
+        dispatch(setCurrentUser(decoded))
+    })
+    .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    )
+}
+
+//set logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(false)
+    dispatch(setCurrentUser({}))
+}
