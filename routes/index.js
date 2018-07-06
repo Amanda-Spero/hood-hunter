5490846f13ef3e066f68da78ead125f71ff8f999
crystalodi <crystalodi@gmail.com>
2018-07-05 22:13:48 -0500

    restructure package.json in client/root. export router in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-/*
+
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+//const apiRoutes = require("./api");
 
 // API Routes
-router.use("/api", apiRoutes);
+//router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
@@ -13,4 +13,3 @@ router.use(function(req, res) {
 
 module.exports = router;
 
-*/
--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-var express = require ("express");
-var mongoose = require ("mongoose");
-var bodyParser = require("body-parser")
-var Neighborhoods = require ("./neighbormodel")
-var app = express();
+const express = require ("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const app = express();
 
 app.use (bodyParser.json())
 app.use (bodyParser.urlencoded({extended: true}))
 
-mongoose.connect("mongodb://hood:hunter4@ds121331.mlab.com:21331/hoodhunterproj");
-app.use(express.static("./client/build"));
+//mongoose.connect("mongodb://hood:hunter4@ds121331.mlab.com:21331/hoodhunterproj");
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
-app.post("/add-saved-neighborhood", function(req, res){
-    const neighborhood = new Neighborhoods (req.body)
-    neighborhood.save().then(function(result){
-        res.json(result);
-    })
+// app.post("/add-saved-neighborhood", function(req, res){
+//     const neighborhood = new Neighborhoods (req.body)
+//     neighborhood.save().then(function(result){
+//         res.json(result);
+//     })
+// })
+
+// app.get("/saved", function(req, res){
+//     Neighborhoods.find()
+//     .exec()
+//     .then(function(doc){
+//         res.send(doc)
+//     })
+// })
+app.use(routes)
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
 })
-
-app.get("/saved", function(req, res){
-    Neighborhoods.find()
-    .exec()
-    .then(function(doc){
-        res.send(doc)
-    })
-})
-
-app.listen(3000);
