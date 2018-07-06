2cd7bc0a911a24df791c623a517dfce2588008a7
crystalodi <crystalodi@gmail.com>
2018-07-06 11:32:37 -0500

    add material-icons css. Change App.js to a component class

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Navbar";
 import MainSearch from "./pages/MainSearch";
 import Footer from "./components/Footer";
 import "./App.css";
 
-const App = () => (
-   
-      <Router>
-        <div className="App Site"> 
-        <div className="App-header">
-            <Nav />
-          </div>
-          <div className="main">
-            <Switch>
-            <Route path="/" component={MainSearch} />
-          </Switch>
-          </div>
-        <Footer/>
+class App extends Component {
+  render() {
+    return (
+    <Router>
+      <div className="App Site"> 
+      <div className="App-header">
+          <Nav />
+        </div>
+        <div className="main">
+          <Switch>
+          <Route path="/" component={MainSearch} />
+        </Switch>
+        </div>
+      <Footer/>
       </div>
-      </Router>
-   
-    );
-
+    </Router>
+    )
+  }
+}
 export default App;
