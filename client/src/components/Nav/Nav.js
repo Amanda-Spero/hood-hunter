48868243f80d27cb70c11a1c20272164e4041cf9
crystalodi <crystalodi@gmail.com>
2018-07-06 17:37:10 -0500

    Render nav menu items based on authentication. add logout link.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { Navbar, NavItem } from 'react-materialize';
-//import "./Navbar.css";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {logoutUser} from "../../actions/authActions";
 
 class Nav extends Component {
   componentDidMount() {
     const el = document.querySelector('.nav-wrapper')
     el.classList.add("container")
+  }
+  logoutLink = e => {
+    this.props.logoutUser()
   }
   render() {
     const styleFont = {
@@ -15,16 +20,33 @@ class Nav extends Component {
     }
     const sideNavOptions = {
         closeOnClick: true
-      }
+    }
+    const {isAuthenticated} = this.props.auth;
+    const unAuthNavBarItems = (
+        <React.Fragment>
+            <NavItem href='/login' style={styleFont.sideNavLinks}>Login</NavItem>
+            <NavItem href='/register' style={styleFont.sideNavLinks}>Register</NavItem>
+        </React.Fragment>
+    )
+    const authNavBarItems = (
+        <React.Fragment>
+            <NavItem href='/' style={styleFont.sideNavLinks}>Saved Searches</NavItem>
+            <NavItem onClick={this.logoutLink} style={styleFont.sideNavLinks}>Logout</NavItem>
+        </React.Fragment>
+    )
     return (
       <Navbar brand='Hood Hunter' right className="blue-grey" options={sideNavOptions}>
-        <NavItem href='/login' style={styleFont.sideNavLinks}>Login</NavItem>
-        <NavItem href='/register' style={styleFont.sideNavLinks}>Register</NavItem>
-        <NavItem href='/' style={styleFont.sideNavLinks}>Saved Searches</NavItem>
-        <NavItem onClick={()=> alert("Logout")} style={styleFont.sideNavLinks}>Logout</NavItem>
+            <NavItem href='/' style={styleFont.sideNavLinks}>Home</NavItem>
+            {isAuthenticated ? authNavBarItems : unAuthNavBarItems}
       </Navbar>
     )
   }
 }
-
-export default Nav;
+Nav.propTypes = {
+    auth: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired
+}
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+export default connect(mapStateToProps, {logoutUser})(Nav);
