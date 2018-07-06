d0dbdaa389b6214c56d57286631366485cca543c
crystalodi <crystalodi@gmail.com>
2018-07-05 22:38:58 -0500

    remove seedDB in scripts Add config folder with passport pass in test db

--- server.js
+++ server.js
@@ -16,21 +16,31 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
-// app.post("/add-saved-neighborhood", function(req, res){
-//     const neighborhood = new Neighborhoods (req.body)
-//     neighborhood.save().then(function(result){
-//         res.json(result);
-//     })
-// })
+/* app.post("/add-saved-neighborhood", function(req, res){
+    const neighborhood = new Neighborhoods (req.body)
+    neighborhood.save().then(function(result){
+        res.json(result);
+    })
+})
 
-// app.get("/saved", function(req, res){
-//     Neighborhoods.find()
-//     .exec()
-//     .then(function(doc){
-//         res.send(doc)
-//     })
-// })
-app.use(routes)
+app.get("/saved", function(req, res){
+    Neighborhoods.find()
+    .exec()
+    .then(function(doc){
+        res.send(doc)
+    })
+}) */
+app.use(passport.initialize())
+
+// Passport Config
+require('./config/passport.js')(passport)
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://crystal:WowNoU89!@ds125031.mlab.com:25031/logindemopj3")
+.then(() => console.log("mongodb connected"))
+.catch(err => console.log(err));
+
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
 })
