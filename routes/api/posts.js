fb2c92d7dc6b646f08bb687c0a72a8483e067642
crystalodi <crystalodi@gmail.com>
2018-07-15 14:44:01 -0500

    add route for deleting a post and adding a comment.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,4 +20,23 @@ router.route("/")
 .post(passport.authenticate('jwt', {session: false}), postsController.createNewPost)
 .get(postsController.getAllPosts)
 
+//@route GET api/posts/:id
+//@desc get a specific post by id
+//access public
+
+//@route DELETE api/posts/:id
+//@desc deletes a post by post id
+//@access private
+router.route("/:id")
+.get(postsController.getPostById)
+.delete(passport.authenticate('jwt', {session: false}), postsController.deletePostById)
+
+//@route POST api/posts/comment/:id
+//@desc comment on a post
+//@desc Private
+router.route("/comment/:id")
+.post(passport.authenticate('jwt', {session: false}), postsController.addCommentToPost)
+
+
+
 module.exports = router;
