9c9ea3cbbbc73dbd93fc35bc8b741cc008c69534
Amanda-Spero <spero1277@gmail.com>
2018-08-17 18:54:28 -0500

    take out react map component

--- client/src/pages/MainSearch/MainSearch.js
+++ client/src/pages/MainSearch/MainSearch.js
@@ -1,11 +1,6 @@
 import React, {Component} from "react";
 import { Container } from "../../components/Grid";
-import Section from "../../components/Section";
-// import MapWithASearchBox from "../../components/SearchBar";
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
-import MapWithASearchBox from "../../components/SearchBar";
-// import SearchResults from "../../components/SearchResults";
-// import SearchForm from "../../components/SearchForm";
 import "./Main.css";
 
 class MainSearch extends Component {
@@ -13,20 +8,6 @@ class MainSearch extends Component {
         return (
           <Container>    
                 <Jumbotron />          
-                {/* <MapWithASearchBox />  */}
-          {/* <div className="container">
-            <div className="section">
-              <div className="row"> */}
-                {/* <div className="col s12 m5">
-              </div>     */}
-                {/* <div className="col s12 m7">       */}
-                  {/* <SearchResults /> */}
-                {/* </div>
-              </div>
-              <div className="resultsRow">
-              </div>
-            </div>                        
-           </div> */}
           </Container>
  
           
